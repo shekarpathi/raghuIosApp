9fe0ddc3a346c149039f84cb764f5d32b271fe83
Shekar <Shekar@yahoo.com>
2019-11-28 11:46:25 -0500

    Initial commit

---
+++ Switch/MyButton.swift
@@ -0,0 +1,38 @@
+//
+//  MyButton.swift
+//  IOT
+//
+//  Created by Lakshmipathi, Shekar on 5/27/18.
+//  Copyright © 2018 Lakshmipathi, Shekar. All rights reserved.
+//
+import UIKit
+
+@IBDesignable class MyButton: UIButton
+{
+    @IBInspectable var cornerRadius: CGFloat = 0{
+        didSet{
+            self.layer.cornerRadius = cornerRadius
+        }
+    }
+    
+    @IBInspectable var borderWidth: CGFloat = 0{
+        didSet{
+            self.layer.borderWidth = borderWidth
+            self.backgroundColor = UIColor.interColor
+        }
+    }
+    /*
+    @IBInspectable var backgroundColor: CGColor = UIColor.orange as! CGColor{
+        didSet{
+            self.layer.borderWidth = borderWidth
+            self.backgroundColor = UIColor.orange
+        }
+     test
+    }*/
+    
+    @IBInspectable var borderColor: UIColor = UIColor.clear{
+        didSet{
+            self.layer.borderColor = borderColor.cgColor
+        }
+    }
+}
---
+++ Switch/UIColorExtensions.swift
@@ -0,0 +1,16 @@
+//
+//  UIColorExtensions.swift
+//  Switch
+//
+//  Created by Shekar on 7/13/19.
+//  Copyright © 2019 Shekar. All rights reserved.
+//
+
+import UIKit
+//https://www.ralfebert.de/ios-examples/uikit/swift-uicolor-picker/
+
+extension UIColor {
+    public static let onColor = UIColor(red: 27/255, green: 230/255, blue: 0/255, alpha: 1.0) /* #1be600 */
+    public static let offColor = UIColor(red: 255/255, green: 20/255, blue: 0/255, alpha: 1.0) /* #ff1400 */
+    public static let interColor = UIColor(red: 0/255, green: 195/255, blue: 209/255, alpha: 1.0) /* #00c3d1 */
+}
