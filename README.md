d223fed4d2456484db7efbf1a0ac5ff4a79a7341
Shekar <Shekar@yahoo.com>
2019-11-29 07:14:17 -0500

    Wrong files

--- README.md
+++
@@ -1,1 +0,0 @@
-# iOS Posting Service
--- Switch/MySession.swift
+++
@@ -1,9 +0,0 @@
-//
-//  MySession.swift
-//  Switch
-//
-//  Created by Shekar on 7/9/19.
-//  Copyright © 2019 Shekar. All rights reserved.
-//
-
-import Foundation
